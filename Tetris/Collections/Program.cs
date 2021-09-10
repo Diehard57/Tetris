3c3e8b95421cafc40c34a1823b9debcf65b2b88e
Diehard57 <32147267+Diehard57@users.noreply.github.com>
2021-09-10 11:59:53 +0300

    .

---
+++ Tetris/Collections/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Tetris;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[5];
+            Point[] points = new Point[3];
+            points[0] = new Point(1, 2, '*');
+            points[1] = new Point(3, 4, '#');
+            points[2] = new Point(5, 6, '*');
+
+            foreach (Point p in points)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
